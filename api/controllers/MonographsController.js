8925439992055394d03cef3926df7c37928e22f8
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-13 17:14:58 -0500

    Modify package.json 'cause add devDependencies; Add monographs controller and service

---
+++ api/controllers/MonographsController.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  list: function (req, res) {
+    return res.send("Hola jotin");
+  },
+  insert: function (req, res) {
+    return res.redirect("http://www.sayonara.com");
+  }
+};
