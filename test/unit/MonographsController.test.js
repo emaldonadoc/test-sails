862772ab64739825bd873d966c2e920d0eedac7f
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-14 14:22:16 -0500

    Init TDD: test in save monograph

--- test/unit/MonographsController.test.js
+++ test/unit/MonographsController.test.js
@@ -1,8 +1,8 @@
 require("sails-test-helper");
 
 describe("Monograph Controller Test", function(){
- describe("GET list", function(){
 
+ describe("GET list", function(){
   it("Should be successful", function(done){
     request.get("/monographs/list")
     .expect(200)
@@ -12,8 +12,25 @@ describe("Monograph Controller Test", function(){
       done();
     });
   });
+ });
 
-
+ describe("PUT monograph",function(){
+   it("Save monograph in DB", function(done){
+     request.put({
+       url:"/monographs/save",
+       headers:{
+         'Content-Type': 'application/json'
+       }
+     })
+     .expect(200)
+     .end(function(err, resp, body){
+       if (err) return done(err);
+       console.log(resp.statusCode, body);
+       done();
+     });
+   });
 
  });
+
+
 });
