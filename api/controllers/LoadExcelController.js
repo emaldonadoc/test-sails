3a463bf5f144a7ba8582ea78ce3556a47dc855ef
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-22 13:03:54 -0500

    First test with no excel path

--- api/controllers/LoadExcelController.js
+++ api/controllers/LoadExcelController.js
@@ -8,7 +8,11 @@
 module.exports = {
 
 	load: function(req, res){
+		if(!req.body.path || res.body.path == "" ){
+			res.statusCode=400;
+			return res.end("Please send a valid excel path");
 
+		}
 		res.end();
 	}
 
