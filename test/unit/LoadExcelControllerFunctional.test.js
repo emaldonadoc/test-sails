f42d9bd6b21eb15c6eefb9e632c21de1e286134d
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-22 13:05:31 -0500

    Add test without body params

--- test/unit/LoadExcelControllerFunctional.test.js
+++ test/unit/LoadExcelControllerFunctional.test.js
@@ -4,8 +4,18 @@ describe("Load Excel Controller Test", function(){
 
   describe("POST load excel", function(){
 
-    it('Return 400 without excel path', function(done){
+    it('Return 400 without body data', function(done){
       request.post('/load')
+       .expect(400)
+       .end(function(err,resp){
+         expect(err).to.not.exist;
+         expect(resp.text).to.equal("Please send a valid excel path");
+         done();
+       });
+    });
+
+    it('Return 400 without excel path', function(done){
+      request.post('/load').send({path:""})
        .expect(400)
        .end(function(err,resp){
          expect(err).to.not.exist;
