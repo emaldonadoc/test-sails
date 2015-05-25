8fa6778d5acf7b9a7111dfb5eaaba0e5bb85a2b3
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-25 12:37:51 -0500

    Add test file

---
+++ test/unit/LoadExcelServiceUnit.test.js
@@ -0,0 +1,16 @@
+require('sails-test-helper');
+
+describe("Load Excel Service Test", function(){
+
+  it("No valid path",function(done){
+    LoadExcelService.readExcel("/excel/data", function(err,data){
+      expect(data).to.not.exist;
+      expect(err).to.exist;
+      expect(err.message).to.equal("No valid file");
+      done();
+    });
+  });
+
+  
+
+});
