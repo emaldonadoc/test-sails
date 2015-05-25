2af80966fa04886e8ef43792feae175583e55b42
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-25 12:37:30 -0500

    File's path no valid

--- api/services/LoadExcelService.js
+++ api/services/LoadExcelService.js
@@ -1,12 +1,27 @@
 var excel = require('excel');
+var path = require('path');
+var excelExtentions = [".xlsx", ".xls"];
 
 var LoadExcelService ={
 
-  readExcel:  function(path, callback){
+  readExcel:  function(pathExcel, callback){
+    var extname = path.extname(pathExcel)
+    if(!isPathNameValid())
+      return callback({message: "No valid file"}, null);
 
+    excel(pathExcel, function(err, data){
+      if(err) return callback(err, null);
+      console.log("DATA READED", data);
+
+    });
   }
 
 }
 
 
 module.exports = LoadExcelService;
+
+
+function isPathNameValid(extname){
+  return excelExtentions.indexOf(extname) > -1
+}
