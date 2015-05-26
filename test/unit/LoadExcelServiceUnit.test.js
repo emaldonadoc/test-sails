af880dfa9cd8b16a0136d3c7890cb5045a2a4e94
Emmanuel Maldonado <emaldonado@innomius.com>
2015-05-26 16:44:00 -0500

    Comment test, only test with local path an file

--- test/unit/LoadExcelServiceUnit.test.js
+++ test/unit/LoadExcelServiceUnit.test.js
@@ -11,15 +11,14 @@ describe("Load Excel Service Test", function(){
     });
   });
 
- it("Read file",function(done){
+ //todo Get out to stub or spy, just test with local data and number xlxs fields
+ it.skip("Read file",function(done){
    var file = "/Users/emmanuelmaldonado/Documents/monografias_test.xlsx";
    LoadExcelService.readExcel(file, function(err, data){
      expect(err).to.not.exist;
      expect(data.loadTotal).to.equal(90);
      done();
    });
-
-
  });
 
 
