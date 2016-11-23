9a4179e3aed33fc2a23dde7b8d8849caad090373
pinky <orochi_alex@hotmail.com>
2016-11-23 13:41:30 -0600

    saved status

--- api/controllers/OptionsController.js
+++ api/controllers/OptionsController.js
@@ -11,72 +11,123 @@ module.exports = {
 		}
 		else{
 			return res.json(
-				{"id_tramite":1,"id_estado":13,"estado":"Hidalgo","id_tipo_pago":2,"id":"57abaf288d25ea58a9000001","fields":[
-				{
+				{"fields":[{
 					"wsList": [
-						 {
-						 "name": "estado",
-						 "label": "Estado",
-						 "class": "form-group col-md-7 required",
-						 "requerido":true,
-						 "class": "form-group col-md-7 required",
-						 "url":"http://10.15.9.2:3000/gobmx/estados",
-						 //"url":"http://localhost:1337/options/{municipio}",
-						 "responseDisplay":"nombre",
-						 "responseValue":"2"
-						 },
-						 {
-						 "name": "otraCosa",
-						 "label": "Municipio",
-						 "requerido":true,
-						 "class": "form-group col-md-7 required",
-						 "url":"http://10.15.9.2:3000/gobmx/municipios?filter[where][id_estado]={estado}and[0traCosa]={otraCosa}",
-						 "responseDisplay":"nombre",
-						 "responseValue":"id_municipio"
-						 }
-
+					{
+						"name": "estado",
+						"label": "Estado",
+						"class": "form-group col-md-7 required",
+						"requerido":true,
+						"class": "form-group col-md-7 required",
+						"url":"http://10.20.58.9/vun/estados",
+						"responseDisplay":"nombre",
+						"responseValue":"id_estado"
+					},
+					{
+						"name": "municipio",
+						"label": "Municipio",
+						"requerido":true,
+						"class": "form-group col-md-7 required",
+						"url":"http://10.20.58.9/vun/municipios?filter[where][id_estado]={estado}",
+						"responseDisplay":"nombre",
+						"responseValue":"id_municipio"
+					}
 					],
-					"class": "form-control form-group col-md-7 ",
-					"required": true,
-					"name": "primer",
-					"type": "select",
+
+					"type": "select-ws",
 					"posicion": 1,
 					"textoApoyo": "Debes seleccionar todo como si tu vida dependiera de ello."
 				},
+
 				{
 					"wsList": [
-						 {
-						 "name": "otraCosa",
-						 "label": "Otra Cosa",
-						 "class": "form-group col-md-7 required",
-						 "requerido":true,
-						 "class": "form-group col-md-7 required",
-						 "url":"http://10.15.9.2:3000/gobmx/estados",
-						 //"url":"http://localhost:1337/options/{municipio}",
-						 "responseDisplay":"nombre",
-						 "responseValue":"id_estado"
-						 },
-						 {
-						 "name": "cosaExtra",
-						 "label": "cosa Extra",
-						 "requerido":true,
-						 "class": "form-group col-md-7 required",
-						 "url":"http://10.15.9.2:3000/gobmx/municipios?filter[where][id_estado]={otraCosa}",
-						 "responseDisplay":"nombre",
-						 "responseValue":"id_municipio"
-						 }
-					 ],
-					"class": "form-control form-group col-md-7 ",
-					"required": true,
-					"name": "segundo",
-					"type": "select",
+					{
+						"name": "municipio",
+						"label": "Municipio",
+						"class": "form-group col-md-7 required",
+						"requerido":true,
+						"class": "form-group col-md-7 required",
+						"url":"http://10.20.58.9/vun/estados",
+						"responseDisplay":"nombre",
+						"responseValue":"id_estado"
+					},
+					{
+						"name": "localidad",
+						"label": "Localidad",
+						"requerido":true,
+						"class": "form-group col-md-7 required",
+						"url":"http://10.20.58.9/vun/municipios?filter[where][id_estado]={municipio}",
+						"responseDisplay":"nombre",
+						"responseValue":"id_municipio"
+					}
+					],
+
+					"type": "select-ws",
 					"posicion": 2,
 					"textoApoyo": "Debes seleccionar todo como si tu vida dependiera de ello."
-				}
+				},
 
-				]}
+				{
+					"name": "estado",
+					"type": "select-ws",
+					"class": "form-group col-md-7",
+					"label": "Estado",
+					"posicion": 4,
+					"options": [
+					{
+						"value": 13,
+						"text": "Hidalgo",
+						"class": "form-group col-md-7",
+						"label": "Municipio",
+						"name": "municipio",
+						"options": [
+						{
+							"value": "tula",
+							"text": "Tula"
+						},
+						{
+							"value": "pachuca",
+							"text": "Pachuca",
+							"class": "form-group col-md-7",
+							"label": "Colonia",
+							"name": "colonia",
+							"options": [
+							{
+								"value": "centro",
+								"text": "Centro"
+							},
+							{
+								"value": "tuzos",
+								"text": "Tuzos"
+							}
+							]
+						}
+						]
+					},
+					{
+						"value": 14,      
+						"text": "Jalisco",
+						"class": "form-group col-md-7",
+						"label": "Departamento",
+						"name":  "departamento",
+						"options": [
+						{
+							"value": "guadalajara",
+							"text": "Guadalajara"
+						},
+						{
+							"value": "tlaquepaque",
+							"text": "Tlaquepaque"
+						}
+						]
+					}
+					]
+				},
+				{"options":[{"value":"opción-1","text":"Opción 1"},{"value":"opción-2","text":"Opción 2"}],"type":"select","required":"true","name":"select-1478884901472","label":"Opciones cmb","class":"form-control form-group col-md-7 ","posicion":5}
 
+
+				]} 
 				);
-		}
-	}
+}
+}
 };
